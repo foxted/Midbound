a8552908748c7d9828f7fa680c54f29a1c440198
alhankeser <alhankeser@gmail.com>
2016-07-14 02:34:54 -0700

    Create left nav and main timeline list styles

--- resources/views/external/styles.blade.php
+++ resources/views/external/styles.blade.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
@@ -17,24 +18,86 @@
         <h2>Heading 2, good for in-app view headings</h2>
         <h3>Heading 3, good for subheadings within the body</h3>
         <p>Kickstarter odio pitchfork laboris, echo park brooklyn gluten-free magna. Irony 3 wolf moon et etsy, labore taxidermy tattooed meditation laborum kale chips stumptown forage chicharrones ad. <a href="#">Drinking vinegar</a> velit gastropub adipisicing pork belly hashtag, direct trade listicle. Odio meditation ramps, enim veniam master cleanse proident kogi sapiente brunch cred brooklyn. Church-key in williamsburg 90's, irony tattooed etsy polaroid elit nesciunt beard occaecat deep v green juice. Velit retro microdosing portland, green juice williamsburg et YOLO. Pour-over aliqua kale chips bitters.</p>
-
-        <h2>Left Nav Style</h2>
-        <div class="col-md-4">
-            <div class="list-group">
-                <div class="list-header">Recent Activity</div>
-                <a href="#" class="list-group-item">Cras justo odio</a>
-                <a href="#" class="list-group-item active">Dapibus ac facilisis in</a>
-                <a href="#" class="list-group-item">Morbi leo risus</a>
-                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
-                <a href="#" class="list-group-item">Vestibulum at eros</a>
-            </div>
-        </div>
-
         <a href="#" class="btn btn-default">Default Button</a>
         <a href="#" class="btn btn-primary">Primary Button</a>
         <a href="#" class="btn btn-success">Success Button</a>
         <a href="#" class="btn btn-danger">Danger Button</a>
         
+        <div class="clearfix"></div>
+        <div class="col-md-3">
+        <h2>Left Nav Style</h2>
+            <div class="list-group">
+                <div class="list-header">My filters</div>
+                <a href="#" class="list-group-item">Cras justo odiasdfasdfasfasdasdfo</a>
+                <a href="#" class="list-group-item active">Dapibus ac facilisasdfasdfasfsdis in</a>
+                <a href="#" class="list-group-item">Morbi leo rifasdfasdfasdfasdsus</a>
+                <a href="#" class="list-group-item">Porta ac consfasdfasdfasdfasdfectetur ac</a>
+                <a href="#" class="list-group-item">Vestibulumasasdfasdfasfd at eros</a>
+                <a href="#" class="list-group-item cta"><i class="fa fa-plus"></i> Add</a>
+            </div>
+        </div>
+       
+        <div class="col-md-9">
+            <h2>List Style</h2>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="prospect">
+                        <div class="avatar"></div>
+                        <div class="info">
+                            <a class="name text-gray" href="#">Virginia Torphy</a><br>
+                            <a class="email" href="#">virginiatorphy@bigcompanyinc.com</a></p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="pull-right"><i class="fa fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="event">
+                       Visited /services/conversion-optimization <span class="time">&mdash; 10 minutes ago</small>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="prospect">
+                        <div class="avatar"></div>
+                        <div class="info">
+                            <a class="name text-gray" href="#">Virginia Torphy</a><br>
+                            <a class="email" href="#">virginiatorphy@bigcompanyinc.com</a></p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="pull-right"><i class="fa fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="event">
+                       Visited /services/conversion-optimization <span class="time">&mdash; 10 minutes ago</small>
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="prospect">
+                        <div class="avatar"></div>
+                        <div class="info">
+                            <a class="name text-gray" href="#">Virginia Torphy</a><br>
+                            <a class="email" href="#">virginiatorphy@bigcompanyinc.com</a></p>
+                        </div>
+                        <div class="actions">
+                            <a href="#" class="pull-right"><i class="fa fa-chevron-right"></i></a>
+                        </div>
+                    </div>
+                    <div class="event">
+                       Visited /services/conversion-optimization <span class="time">&mdash; 10 minutes ago</small>
+                    </div>
+                </div>
+            </div>
+</div>
+        
+        
+            
+        
+        
+        
+
     </div>
 </body>
 </html>
