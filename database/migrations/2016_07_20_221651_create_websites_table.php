e8d6bcd719e2f9f3ec63b7f03304b7f66b9fdd14
Valentin Prugnaud <valentin@whatdafox.com>
2016-07-24 14:46:27 -0700

    Create website model and table

---
+++ database/migrations/2016_07_20_221651_create_websites_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWebsitesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('websites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('team_id')->unsigned()->index();
+            $table->string('mbid')->unique();
+            $table->string('name');
+            $table->string('url');
+            $table->timestampsTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('websites');
+    }
+}
