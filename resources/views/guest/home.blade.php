0e95a15b1764769de7abc20b88d949d36b05bbe7
alhankeser <alhankeser@gmail.com>
2016-07-25 15:36:37 -0700

    Setup of landing page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html lang="en" id="welcome">
-<head>
-    <!-- Meta Information -->
-    <title>Midbound</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" href="{{ asset('favicon-32x32.png') }}" sizes="32x32">
-    <link rel="icon" type="image/png" href="{{ asset('favicon-16x16.png') }}" sizes="16x16">
-</head>
-<body>
-    <div class="flex-fill flex-center">
-        <h1 class="text-center">
-            <img src="{{ asset('img/midbound-logo-600.png') }}">
-        </h1>
-    </div>
-</body>
-</html>
+@extends('spark::layouts.app')
+
+@section('content')
+<div class="home-banner">
+    
+</div>
+@endsection
