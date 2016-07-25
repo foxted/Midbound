b98a23d163b513f421302b24d7eb31e1bdecf86b
alhankeser <alhankeser@gmail.com>
2016-07-25 16:53:16 -0700

    Complete basic landing page

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 <div class="home-banner">
+    <h1>Real-time B2B Prospecting</h1>
+    <p class="lead">Midbound tells you which sales prospects matter and when to make your move.</p>
+    <form class="form-inline" method="POST" action="">
+        {{ csrf_field() }}
+        <div class="form-group">
+            <input type="text" name="email" id="inputemail" class="form-control" value="" placeholder="Enter your email">
+        </div>
+        <button type="submit" class="btn btn-primary btn-lg">Get Early Access</button>
+    </form>
     
 </div>
 @endsection
