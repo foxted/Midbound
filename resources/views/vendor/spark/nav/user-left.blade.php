ee3297a4df687521996b8691d5cb92b8c633d336
alhankeser <alhankeser@gmail.com>
2016-07-31 16:58:27 -0700

    Advanced search view

--- resources/views/vendor/spark/nav/user-left.blade.php
+++ resources/views/vendor/spark/nav/user-left.blade.php
@@ -6,9 +6,41 @@
 </li>
 <form class="navbar-form navbar-left search"> 
 	<div class="form-group"> 
-		<input class="form-control" placeholder="Search for anything..."> 
+		<input class="form-control" onclick="showAdvancedSearch();" placeholder="Search for anything..."> 
 	</div> 
-	<button type="submit" class="btn btn-lg btn-default">
-		<i class="fa fa-search"></i>
-	</button> 
+	<!-- Advanced Search Panel -->
+       <div class="panel advanced-search" id="advancedSearchPanel" style="display:none;">
+          <div class="panel-body">
+          	<div class="list-group">
+                    <div class="list-header">Type</div>
+                    <a href="#" class="list-group-item active">Prospects</a>
+                    <a href="#" class="list-group-item disabled">Companies</a>
+                    <a href="#" class="list-group-item disabled">Campaigns</a>
+                    <a href="#" class="list-group-item disabled">Tasks</a>
+              </div>
+              <div class="list-group">
+                    <div class="list-header">Assigned To</div>
+                    <a href="#" class="list-group-item active">Me</a>
+                    <a href="#" class="list-group-item">Others</a>
+                    <a href="#" class="list-group-item">Not assigned</a>
+                    <a href="#" class="list-group-item">Any</a>
+                </div>
+                <div class="list-group">
+                    <div class="list-header">Score</div>
+                    <a href="#" class="list-group-item">Low</a>
+                    <a href="#" class="list-group-item">Medium</a>
+                    <a href="#" class="list-group-item active">High</a>
+                    <a href="#" class="list-group-item">Any</a>
+                </div>
+          </div>
+          <div class="panel-footer">
+          	<button class="btn btn-primary" type="submit">Search</button>
+          	<a class="btn btn-ghost">Save this Search</a>
+          	<div class="pull-right">
+              		<a class="btn btn-ghost" href="#"> Cancel</a>
+              </div>
+          </div>
+       </div>
 </form>
+
+
