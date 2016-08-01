f1ee4881650bd49dae3e02854e4e5860e53100a9
alhankeser <alhankeser@gmail.com>
2016-08-01 13:24:15 -0700

    Guest home view benefits modules design

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -28,12 +28,29 @@
     		<div class="panel-body">
     			<div class="description">
     				<h2>See what prospects are doing <em>right now</em></h2>
-    				Research shows that response rate is where the money's at. With Midbound, you'll have real-time data on who is doing what so you can act immediately.
+    				<p>Research shows that response rate is where the money's at. With Midbound, you'll have real-time data on who is doing what so you can act immediately.</p>
+                    <a href="/features/real-time">Learn more</a>
     			</div>
     		<div class="img-preview">
-    			<img class="img-responsive" src="/img/midbound-feature-real-time.jpg">
+    			<a href="/features/real-time">
+                    <img class="img-responsive" src="/img/midbound-feature-real-time.jpg">
+                </a>
     		</div>
     		</div>
+    </div>
+    <div class="panel panel-benefit right">
+            <div class="panel-body">
+                <div class="description">
+                    <h2>Focus on prospects who <em>actually matter</em></h2>
+                    <p>There's only so much time in a day. With Midbound, you'll spend that time effectively by putting your energy behind the highest value prospects, at the right time. We automatically score prospects by default and you can create a custom scoring system.</p>
+                    <a href="/features/scoring">Learn more</a>
+                </div>
+            <div class="img-preview">
+                <a href="/features/scoring">
+                    <img class="img-responsive" src="/img/midbound-feature-scoring.jpg">
+                </a>
+            </div>
+            </div>
     </div>
 </div>
 </div>
