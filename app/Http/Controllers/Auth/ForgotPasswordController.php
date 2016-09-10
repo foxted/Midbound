8d8c0192dfa297522b230c0e9e924de54105ed71
Laravel Shift <shift@laravelshift.com>
2016-09-10 01:25:55 -0400

    Shift cleanup

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Midbound\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Midbound\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
 class ForgotPasswordController extends Controller
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\Auth;
+namespace Midbound\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Midbound\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ResetsPasswords;
 
 class ResetPasswordController extends Controller
