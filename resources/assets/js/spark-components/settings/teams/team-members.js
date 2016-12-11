c212427c0f357581c220c60a6d6d0be71e2f5f97
Tony Ta <tonyta78@gmail.com>
2016-12-11 14:50:00 -0800

    Allow member to delete non-owner members

--- resources/assets/js/spark-components/settings/teams/team-members.js
+++ resources/assets/js/spark-components/settings/teams/team-members.js
@@ -1,5 +1,17 @@
 var base = require('settings/teams/team-members');
 
 Vue.component('spark-team-members', {
-    mixins: [base]
+    mixins: [base],
+    methods: {
+        /**
+         * Determine if the current user can delete a team member.
+         */
+        canDeleteTeamMember(member) {
+        	if (member.pivot.role == "member") {
+        		return this.user.id !== member.id;
+        	}  else {
+        		return this.user.id === this.team.owner_id && this.user.id !== member.id
+        	}
+        }
+    }
 });
